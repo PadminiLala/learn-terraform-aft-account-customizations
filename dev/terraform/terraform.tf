555b0871f0e720ddfd893cc042555bdfa3f091f3
PadminiLala <plala0901@gmail.com>
2024-10-07 10:57:53 -0400

    ipam automation

--- dev/terraform/terraform.tf
+++ dev/terraform/terraform.tf
@@ -15,7 +15,7 @@ terraform {
 provider "infoblox" {
   server = "34.199.124.91"
   username = "admin"
-  password = "2p#MiT=-Sq#B7P2"
+  password = nonsensitive(data.aws_ssm_parameters_by_path.infoblox_parms.values)
   sslmode = false
 }
 data "aws_ssm_parameters_by_path" "infoblox_parms" {
